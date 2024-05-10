c657ef83840ab1bc512af2efa73aa963353f90f5
Nick <nikdanelia@gmail.com>
2024-05-10 22:27:36 +0400

    initial commit

---
+++ apptype/apptype.go
@@ -0,0 +1,26 @@
+package apptype
+
+import (
+	"image/color"
+
+	"fyne.io/fyne/v2"
+)
+
+type BrushType = int
+
+type PxCanvasConfig struct {
+	DrawingArea    fyne.Size
+	CanvasOffset   fyne.Position
+	PxRows, PxCols int
+	PxSize         int
+}
+type State struct {
+	BrushColor     color.Color
+	brushType      int
+	SwatchSelected int
+	FilePath       string
+}
+
+func (state *State) SetFilePath(path string) {
+	state.FilePath = path
+}
